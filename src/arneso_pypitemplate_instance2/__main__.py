156210062174cde66fd2c33a7e45d32444fe3a42
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2023-11-15 10:58:33 +0100

    Initial commit

---
+++ src/arneso_pypitemplate_instance2/__main__.py
@@ -0,0 +1,12 @@
+"""Command-line interface."""
+import click
+
+
+@click.command()
+@click.version_option()
+def main() -> None:
+    """Arneso Pypitemplate Instance2."""
+
+
+if __name__ == "__main__":
+    main(prog_name="arneso-pypitemplate-instance2")  # pragma: no cover
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Test suite for the arneso_pypitemplate_instance2 package."""
