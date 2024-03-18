cf244186123838b93d2ea7c8f676e0010a7a988a
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2024-03-18 09:54:31 +0100

    Add testcase for testing SonarCloud

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,14 @@
+from arneso_pypitemplate_instance2.functions import append_to_list
 from arneso_pypitemplate_instance2.functions import example_function
 
 
 def test_example_function() -> None:
     assert example_function(1, 2) == "1 is less than 2"
     assert example_function(1, 0) == "1 is greater than or equal to 0"
+
+
+def test_append_to_list() -> None:
+    result1 = append_to_list(12)
+    result2 = append_to_list(101, [1, 2, 3])
+    result3 = append_to_list(102)
+    # The result3 is [12, 102], because of mutable default value
